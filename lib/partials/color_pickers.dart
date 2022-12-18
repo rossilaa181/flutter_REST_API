bec27339f681a8c47e7697de8a05a10c6ff34a5d
rossilaa181 <latansasalsabela@gmail.com>
2022-12-19 03:33:27 +0700

    add asset color & font pickers

---
+++ lib/partials/color_pickers.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ColorPicker {
+  static const primary = Color(0xFF3F51B5);
+  static const primaryLight = Color(0xFF4B5CBB);
+  static const primaryShape = Color.fromARGB(255, 45, 58, 133);
+  static const orangeColor = Color(0xffFFA26C);
+  static const danger = Color.fromARGB(255, 255, 50, 88);
+  static const white = Color(0xffffffff);
+  static const bg = Color(0xffE5ECF0);
+  static const black = Color(0xff000000);
+  static const dark = Color(0xff2F2F2F);
+  static const hintText = Color(0xffdddddd);
+  static const grey = Color.fromARGB(255, 103, 103, 103);
+}
---
+++ lib/partials/font_pickers.dart
@@ -0,0 +1,6 @@
+class FontPicker{
+  static const regular = 'Montserrat-Regular';
+  static const medium = 'Montserrat-Medium';
+  static const semibold = 'Montserrat-SemiBold';
+  static const bold = 'Montserrat-Bold';
+}
