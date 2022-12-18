f5f2a5b65f623549fd125528732f15a5a104b374
rossilaa181 <latansasalsabela@gmail.com>
2022-12-19 03:38:39 +0700

    update home screen

--- lib/Pages/home_page.dart
+++ lib/Pages/home_page.dart
@@ -1,11 +1,17 @@
 import 'dart:convert';
 import 'package:provider/provider.dart';
+import 'package:get/get.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
+import 'package:flutter_api/controllers/user_controller.dart';
 import 'package:flutter_api/Services/auth_services.dart';
 import 'package:flutter_api/Pages/login_screen.dart';
+import 'package:flutter_api/Pages/category_screen.dart';
+import 'package:flutter_api/partials/color_pickers.dart';
+import 'package:flutter_api/partials/font_pickers.dart';
 
 import 'package:http/http.dart' as http;
 
@@ -23,6 +29,11 @@ class _HomePageState extends State<HomePage> {
     super.initState();
     // context.read<CategoryService>.categoryList();
   }
+
+  static Future<SharedPreferences> _prefs = SharedPreferences.getInstance();
+  UserController userController = Get.put(UserController());
+
+  bool _isHome = true;
 
   logoutPressed() async {
     http.Response response = await AuthServices.logout();
@@ -44,35 +55,109 @@ class _HomePageState extends State<HomePage> {
     }
   }
 
+  home() async {
+    if (_isHome == true) {
+      Navigator.pushAndRemoveUntil(
+        context,
+        MaterialPageRoute(
+          builder: (BuildContext context) => const HomePage(),
+        ),
+        (route) => false,
+      );
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("Home Page"),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(8.0),
+      body: SingleChildScrollView(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-            Container(
-              child: Text(
-                "Welcome, superadmin@gmail.com!",
-                style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                  fontSize: 20,
+            Stack(
+              children: [
+                Container(
+                  padding: const EdgeInsets.all(0),
+                  height: 280,
+                  width: double.infinity,
                 ),
-              ),
-            ),
-            SizedBox(
-              height: 50.0,
+                Container(
+                  padding: const EdgeInsets.all(10),
+                  height: 200,
+                  decoration: const BoxDecoration(
+                      color: ColorPicker.primary,
+                      borderRadius:
+                          BorderRadius.only(bottomLeft: Radius.circular(80))),
+                ),
+                Positioned(
+                  left: 25,
+                  top: 80,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      FutureBuilder(
+                        future: userController.getName(),
+                        builder: (context, snapshot) => Text(
+                          'Hai, ${snapshot.data}',
+                          style: const TextStyle(
+                              fontFamily: FontPicker.bold,
+                              fontSize: 20,
+                              color: ColorPicker.white),
+                        ),
+                      ),
+                      const Align(
+                        child: Text(
+                          'Stisla User',
+                          style: TextStyle(
+                              fontFamily: FontPicker.medium,
+                              fontSize: 14,
+                              color: ColorPicker.white),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                Positioned(
+                  left: 10,
+                  top: -30,
+                  child: Container(
+                    padding: const EdgeInsets.all(10),
+                    height: 100,
+                    width: 100,
+                    decoration: BoxDecoration(
+                        color: ColorPicker.primaryShape,
+                        borderRadius: BorderRadius.circular(80)),
+                  ),
+                ),
+                Positioned(
+                  right: 25,
+                  top: 80,
+                  child: Container(
+                    padding: const EdgeInsets.all(0),
+                    height: 40,
+                    width: 120,
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10),
+                        color: ColorPicker.white),
+                    child: TextButton(
+                      onPressed: home,
+                      child: const Text(
+                        'Home',
+                        style: TextStyle(
+                            fontFamily: FontPicker.semibold,
+                            color: ColorPicker.primary),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
             ),
             Row(
               crossAxisAlignment: CrossAxisAlignment.center,
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                // Image.asset('assets/images/logo.png'),
+                Image.asset('assets/images/logo.png'),
               ],
             ),
             SizedBox(
@@ -85,10 +170,10 @@ class _HomePageState extends State<HomePage> {
               // convert button
               child: ElevatedButton(
                 onPressed: () {
-                  // Navigator.push(
-                  //   context,
-                  //   MaterialPageRoute(builder: (context) => const ListPage()),
-                  // );
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => const HomeScreen()),
+                  );
                 },
                 child: Text('Category List'),
               ),
--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
 import 'package:flutter_api/Pages/login_screen.dart';
-import 'package:flutter_api/Pages/register_page.dart';
-import 'package:flutter_api/Pages/login_page.dart';
+import 'package:flutter_api/Pages/register_screen.dart';
+import 'package:flutter_api/Pages/auth_screen.dart';
 import 'package:flutter_api/Pages/home_page.dart';
+import 'package:flutter_api/Splash/splash.dart';
 
 void main() {
   runApp(const MyApp());
@@ -17,9 +18,12 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       routes: {
-        "/": (context) => const LoginScreen(),
+        "/": (context) => const SplashScreen(),
         // "/listpage": (context) => const ListPage(),
+        "/authpage": (context) => const AuthScreen(),
         "/homepage": (context) => const HomePage(),
+        "/registerpage": (context) => const RegisterScreen(),
+        "/loginpage": (context) => const LoginScreen(),
       },
       initialRoute: "/",
       title: 'Flutter Demo',
