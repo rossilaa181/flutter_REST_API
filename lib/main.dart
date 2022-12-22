6bbb7cfcd3365014a2d72435dda9829dfa1eab91
rossilaa181 <latansasalsabela@gmail.com>
2022-12-22 10:30:06 +0700

    fix route

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,7 @@ import 'package:flutter_api/Pages/login_screen.dart';
 import 'package:flutter_api/Pages/register_screen.dart';
 import 'package:flutter_api/Pages/auth_screen.dart';
 import 'package:flutter_api/Pages/home_page.dart';
+import 'package:flutter_api/Pages/category_screen.dart';
 import 'package:flutter_api/Splash/splash.dart';
 
 void main() {
@@ -20,10 +21,11 @@ class MyApp extends StatelessWidget {
       routes: {
         "/": (context) => const SplashScreen(),
         // "/listpage": (context) => const ListPage(),
-        "/authpage": (context) => const AuthScreen(),
-        "/homepage": (context) => const HomePage(),
-        "/registerpage": (context) => const RegisterScreen(),
-        "/loginpage": (context) => const LoginScreen(),
+        "/auth": (context) => const AuthScreen(),
+        "/home": (context) => const HomePage(),
+        "/register": (context) => const RegisterScreen(),
+        "/login": (context) => const LoginScreen(),
+        "/category": (context) => const CategoryScreen(),
       },
       initialRoute: "/",
       title: 'Flutter Demo',
